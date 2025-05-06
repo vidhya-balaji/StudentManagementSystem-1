013a512ea538cb8ec543fd014755ce466fe20b74
vidhya-balaji <vidhyabalaji.nina@gmail.com>
2025-05-06 22:41:23 +0530

    cc

--- backend/index.js
+++ backend/index.js
@@ -43,15 +43,14 @@ app.get("/login",function(req,res)
     // console.log(req.query.password);
     userdata.map((data)=>
     {
-        res.send(true)
-        // if(data.username===req.query.UserName&&data.password===req.query.password)
-        // {
-        //     res.send(true)
-        // }
-        // else
-        // {
-        //     res.send(false)
-        // }
+        if(data.username===req.query.UserName)
+        {
+            res.send(true)
+        }
+        else
+        {
+            res.send(false)
+        }
     })
 
     
